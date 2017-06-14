c410784f4030fc1c0dbbd83115dd7cd91e11082b
Alan Orth <alan.orth@gmail.com>
2017-06-14 14:05:10 +0300

    Sync more stuff from production

---
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/partners-collector.js
@@ -0,0 +1,60 @@
+function prepareNames($inputs){
+	var partners = [];
+	//Fix names
+	for(var i =0; i< $inputs.length;i++){
+		var partner = $inputs[i].value;
+		if(partner == "Not Applicable")
+			continue;
+        partner = partner.replace(/ *\([^)]*\) */g, ""); //Remove brackets 
+        partner = partner.replace(/ *\-[^]*\ */g, ""); //Remove dashes
+        partners.push(partner);
+	}
+
+	//Remove duplicates
+	partners = partners.filter( function( item, index, inputArray ) {
+           return inputArray.indexOf(item) == index;
+    });
+
+    return partners;
+}
+
+function requestLogos(partners, callback){
+	var images = [];
+	var index = 0;
+	for (var i = 0; i < partners.length; i++) {
+		jQuery.ajax({
+		     url:"https://mel.cgiar.org/api/partners/" + encodeURIComponent(partners[i]),
+		     data: {client_id:1, client_secret: 'hello'},
+		     dataType: 'jsonp',
+		     success:function(json){
+		     	 index++;
+		         if(json.logo && json.logo.length > 38){//be sure logo available 
+		            images.push(json.logo);
+		         }
+		         if(index == partners.length){
+		            callback(images);
+		         }
+		     },
+		     error:function(){
+		     	index++;
+		     }      
+		});
+	}
+}
+
+function appendImages(images){
+	if(images.length > 0){
+		jQuery('.partners-list .logos').prepend('<h5>Partners</h5>');
+	}
+	for(var i = 0; i < images.length; i++){
+		jQuery('.partners-list .logos').append('<img src="' + images[i] + '" />');
+	}
+}
+
+function checkPartners(){
+  if(jQuery('.partners-list').length > 0){
+     $inputs = jQuery('.partners-list .values input');
+     var partners = prepareNames($inputs);
+     requestLogos(partners, appendImages);
+  }
+}
