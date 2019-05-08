21e51ddedb3a3b30a63e4711a449f79cadf665f4
mohammadsalem <mohammad.salem231@gmail.com>
2019-05-09 02:26:01 +0300

    Add log files volumes, docker ignore update and database import/export update

--- DSPACE_BACKUP.sh
+++ DSPACE_BACKUP.sh
@@ -3,10 +3,10 @@ db_name="dspace-$(date +%s).dump"
 
 echo "$(tput setaf 6)Creating full backup $db_name.tar.gz ..."$(tput sgr 0)
 
-sudo docker exec -i dspace_db pg_dump -U dspace -Fc -f /var/lib/postgresql/data/$db_name dspace
-cd /home/ubuntu/dspace-docker/db_backup/
-sudo mv /home/ubuntu/dspace-docker/postgresData/$db_name .
-sudo tar -czvf $db_name.tar.gz $db_name
-sudo rm /home/ubuntu/db_backup/$db_name
+docker exec -i dspace_db pg_dump -U dspace -Fc -f /$db_name dspace
+cd /home/ubuntu/db_backup/
+docker cp dspace_db:/$db_name .
+tar -czvf $db_name.tar.gz $db_name
+rm $db_name
 echo "$(tput setaf 6)Backup finished."$(tput sgr 0)
 
--- IMPORT_DSPACE_DB.sh
+++ IMPORT_DSPACE_DB.sh
@@ -45,10 +45,10 @@ echo "$(tput setaf 6)Creating dspace user..."$(tput sgr 0)
 docker exec dspace_db psql -U postgres dspace -c 'alter user dspace createuser;'
 
 echo "$(tput setaf 6)Copying database..."$(tput sgr 0)
-cp dspace-$num.dump /home/ubuntu/dspace-docker/postgresData/
+docker cp dspace-$num.dump dspace_db:/
 
 echo "$(tput setaf 6)Importing database..."$(tput sgr 0)
-docker exec dspace_db pg_restore -U postgres -d dspace /var/lib/postgresql/data/dspace-$num.dump
+docker exec dspace_db pg_restore -U postgres -d dspace /dspace-$num.dump
 
 echo "$(tput setaf 6)Removing dspace user..."$(tput sgr 0)
 docker exec dspace_db psql -U postgres dspace -c 'alter user dspace nocreateuser;'
@@ -57,12 +57,13 @@ echo "$(tput setaf 6)Vacum database..."$(tput sgr 0)
 docker exec dspace_db vacuumdb -U postgres dspace
 
 echo "$(tput setaf 6)Updating sequences..."$(tput sgr 0)
-docker cp dspace:/dspace/etc/postgres/update-sequences.sql /home/ubuntu/dspace-docker/postgresData/
-docker exec dspace_db psql -U dspace -f /var/lib/postgresql/data/update-sequences.sql dspace
+docker cp dspace:/dspace/etc/postgres/update-sequences.sql .
+docker cp update-sequences.sql dspace_db:/
+docker exec dspace_db psql -U dspace -f /update-sequences.sql dspace
 
 echo "$(tput setaf 6)Cleaning up..."$(tput sgr 0)
-rm /home/ubuntu/dspace-docker/postgresData/update-sequences.sql
-rm /home/ubuntu/dspace-docker/postgresData/dspace-$num.dump
+docker exec -it dspace_db bash -c "rm dspace-1545868801.dump"
+docker exec -it dspace_db bash -c "rm update-sequences.sql"
 rm dspace-$num.dump
 
 echo "$(tput setaf 6)Starting dspace container..."$(tput sgr 0)
